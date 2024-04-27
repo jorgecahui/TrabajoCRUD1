c78e91b0de745800fecf74cc5e9bcc317a09306b
jorgecahui <jorgecahui347@gmail.com>
2024-04-27 18:04:40 -0500

    proyectocrud

---
+++ src/main/java/com/mycompany/crudarraylist/CRUDArraylist.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.crudarraylist;
+
+/**
+ *
+ * @author asus
+ */
+public class CRUDArraylist {
+
+    public static void main(String[] args) {
+        
+        Vista vista = new Vista();
+        vista.setVisible(true);
+        
+    }
+}
